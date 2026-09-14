45fe8ffd291561748bf9dd13f020cc29f68056d7
Juan Mizzi <juan.mizzi.1@gov.mt>
2026-09-14 09:06:25 +0200

    Update config.js

--- github-pages-worklog/config.js
+++ github-pages-worklog/config.js
@@ -10,7 +10,7 @@ window.WORKLOG_CONFIG = window.WORKLOG_CONFIG || {
     measurementId: "G-3T01YPHH66"
   },
   jiraWorkerUrl: "https://jira-proxy.juan-mizzi.workers.dev",
-  sprintSummaryUrl: "https://1e24479cee97eedd82feff45823fa9.59.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/28/workflows/da0fcc244adb484298bd792069fed5c3/triggers/manual/paths/invoke?api-version=1",
+  sprintSummaryUrl: "https://1e24479cee97eedd82feff45823fa9.59.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/28/workflows/da0fcc244adb484298bd792069fed5c3/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=cYILv4nO_Ic2GFQPO4Qq_Scc3Hi2DtdGpzrAo6g-YTk",
   pbiDraftUrl: "",
   uatApiUrl: ""
 };
